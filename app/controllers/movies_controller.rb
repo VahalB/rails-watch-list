5cab3181a409006fd123dfa36c63f76e1de8519b
Valentin Beau <valentinbeau.fr@gmail.com>
2023-02-24 14:59:21 +0100

    lil save

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -43,3 +43,4 @@ class MoviesController < ApplicationController
     end
   
   end
+  
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,26 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+require 'open-uri'
+require 'json'
+
+puts "Cleaning up database..."
 Movie.destroy_all
-url = "https://tmdb.lewagon.com/movie/top_rated"
-movies = JSON.parse(URI.open(url).read)
-movies["results"].take(30).each do |movie|
-  Movie.create!(title: movie["original_title"], poster_url: movie["poster_path"], overview: movie["overview"], rating: movie["vote_average"])
+puts "Database cleaned"
+
+url = "http://tmdb.lewagon.com/movie/top_rated"
+10.times do |i|
+  puts "Importing movies from page #{i + 1}"
+  movies = JSON.parse(URI.open("#{url}?page=#{i + 1}").read)['results']
+  movies.each do |movie|
+    puts "Creating #{movie['title']}"
+    base_poster_url = "https://image.tmdb.org/t/p/original"
+    Movie.create(
+      title: movie['title'],
+      overview: movie['overview'],
+      poster_url: "#{base_poster_url}#{movie['backdrop_path']}",
+      rating: movie['vote_average']
+    )
+  end
 end
+puts "Movies created"
